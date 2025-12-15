a7e1977cb491cf629ecff3c7436e9fdf0f3f5112
ravindu644 <droidcasts@protonmail.com>
2025-12-15 21:00:15 +0530

    build script: introduce initial empty nethunter.config
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build_pixel_kernel.sh
+++ build_pixel_kernel.sh
@@ -52,7 +52,7 @@ export BUILD_OPTIONS=(
 
 build_kernel(){
     # Make default configuration.
-    make "${BUILD_OPTIONS[@]}" pixel_marlin_defconfig custom.config
+    make "${BUILD_OPTIONS[@]}" pixel_marlin_defconfig custom.config nethunter.config
 
     # Configure the kernel (GUI)
     make "${BUILD_OPTIONS[@]}" menuconfig
